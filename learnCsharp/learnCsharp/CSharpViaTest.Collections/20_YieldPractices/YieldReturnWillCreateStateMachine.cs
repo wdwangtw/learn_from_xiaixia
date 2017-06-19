ae605fb52e875e4ef82da5d3af70c123d5f68cd3
hunters <bjchen@thoughtworks.com>
2017-06-19 09:45:58 +0800

    add xiaxia's test file, compile success

---
+++ learnCsharp/learnCsharp/CSharpViaTest.Collections/20_YieldPractices/YieldReturnWillCreateStateMachine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CSharpViaTest.Collections._20_YieldPractices
+{
+    /* 
+     * Description
+     * ===========
+     * 
+     * This is the first test you have to complete to understand how to use "yield" keyword.
+     * 
+     * Difficulty: Super Easy
+     * 
+     * Knowledge Point
+     * ===============
+     * 
+     * - We can implicitly implement IEnumerator<T> using `yield` keyword.
+     * - You can use string constructor or `StringBuilder` to quickly create string with
+     *   repeating characters.
+     * 
+     * Requirement
+     * ===========
+     * 
+     * - No LINQ method is allowed to use in this test.
+     */
+    public class YieldReturnWillCreateStateMachine
+    {
+        #region Please modifies the code to pass the test 
+
+        public IEnumerable<string> GetStringTriangle(char character, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        [Fact]
+        public void should_get_skipped_sequences()
+        {
+            IEnumerable<string> enumerable = GetStringTriangle('*', 4);
+            string[] expected =
+            {
+                "*",
+                "**",
+                "***",
+                "****"
+            };
+
+            Assert.Equal(expected, enumerable);
+        }
+
+        [Fact]
+        public void should_returns_enumerable_rather_than_collection()
+        {
+            IEnumerable<string> enumerable = GetStringTriangle('*', 2);
+            Assert.False(enumerable is ICollection<string>);
+        }
+    }
+}
